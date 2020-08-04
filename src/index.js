70fbdad167a99e42131a76babbc6c02c129e3606
María Pérez <maria_erez@hotmail.es>
2020-08-04 17:03:43 -0500

    commit2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 ReactDOM.render(<h1>hola platzi</h1>, document.getElementById('app'))
