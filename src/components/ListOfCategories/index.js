3d36570825d59c69ef6b911773199297d8208ebf
María Pérez <maria_erez@hotmail.es>
2020-08-05 22:18:39 -0500

    polifill e imports dinamicos

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,18 +1,51 @@
 import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
-import { categories as mockCategories } from '../../../api/db.json'
 import { List, Item } from './styles'
 
-export const ListOfCategories = () => {
-  const [categories, setCategories] = useState(mockCategories)
+function useCategoriesData () {
+  const [categories, setCategories] = useState([])
+  const [loading, setLoading] = useState(false)
   useEffect(function () {
-    fetch('')
-  })
-  return (
-    <List>
+    setLoading(true)
+    window.fetch('https://curso-platzi-react-avanzado.maria-perez123.vercel.app/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+        setLoading(false)
+      })
+  }, [])
+  return { categories, loading }
+}
+
+export const ListOfCategories = () => {
+  const { categories, loading } = useCategoriesData()
+  const [showFixed, setshowFixed] = useState(false)
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed &&
+      setshowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
       {
-        categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+        loading
+          ? <Item key='loading'><Category /></Item>
+          : categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,22 +1,51 @@
-import React from 'react'
-import { ImgWrapper, Button, Img } from './styles'
+import React, { useEffect, useRef, useState } from 'react'
+import { Article, ImgWrapper, Button, Img } from './styles'
 import { MdFavoriteBorder } from 'react-icons/md'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  const element = useRef(null)
+  const [show, setShow] = useState(false)
+
+  useEffect(function () {
+    Promise.resolve(
+      typeof window.IntersectionObserver !== 'undefined'
+        ? window.IntersectionObserver
+        // import dinamico
+        : import('intersection-observer')
+    ).then(() => {
+      // console.log(element)
+      const observer = new window.IntersectionObserver(function (entries) {
+        const { isIntersecting } = entries[0]
+        // console.log({ isIntersecting })
+        if (isIntersecting) {
+          console.log('si')
+          setShow(true)
+          observer.disconnect()
+        }
+      })
+      observer.observe(element.current)
+    })
+  }, [element.current])
+
   return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} />
-        </ImgWrapper>
-      </a>
+    <Article ref={element}>
+      {
+        show &&
+          <>
+            <a href={`/detail/${id}`}>
+              <ImgWrapper>
+                <Img src={src} />
+              </ImgWrapper>
+            </a>
 
-      <Button>
-        <MdFavoriteBorder size='32px' />{likes} likes!
-      </Button>
+            <Button>
+              <MdFavoriteBorder size='32px' />{likes} likes!
+            </Button>
+          </>
+      }
 
-    </article>
+    </Article>
   )
 }
