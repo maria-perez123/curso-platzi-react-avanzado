bd01239d021a5f722ae0dc6ce8b130d12cbbfd2f
María Pérez <maria_erez@hotmail.es>
2020-08-07 19:52:38 -0500

    mutations

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import { GlobalStyles } from './Styles/GlobalStyles'
 import { ListOfCategories } from './components/ListOfCategories'
-import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { ListOfPhotoCards } from './container/ListOfPhotoCards'
 import { Logo } from './components/Logo'
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 
-export const App = () => (
-  <>
-    <Logo />
-    <GlobalStyles />
-    <ListOfCategories />
-    <ListOfPhotoCards />
-  </>
-)
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get('detail')
+  return (
+
+    <div>
+      <GlobalStyles />
+      <Logo />
+      {
+        detailId
+          ? <PhotoCardWithQuery id={detailId} />
+          : <><ListOfCategories />
+            <ListOfPhotoCards categoryId={2} />
+          </>
+      }
+    </div>
+
+  )
+}
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Article, ImgWrapper, Button, Img } from './styles'
-import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { Article, ImgWrapper, Img } from './styles'
 import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
-
+import { FavButton } from '../FavButton'
+import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
@@ -11,24 +11,30 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const key = `like-${id}`
   const [liked, setLiked] = useLocalStorage(key, false)
 
-  console.log(liked)
-
-  const Icon = liked ? MdFavorite : MdFavoriteBorder
+  // console.log(liked)
 
   return (
     <Article ref={element}>
       {
         show &&
           <>
-            <a href={`/detail/${id}`}>
+            <a href={`/?detail=${id}`}>
               <ImgWrapper>
                 <Img src={src} />
               </ImgWrapper>
             </a>
+            <ToggleLikeMutation>
+              {
+                (toggleLike) => {
+                  const handleFavClick = () => {
+                    !liked && toggleLike({ variables: { input: { id } } })
+                    setLiked(!liked)
+                  }
+                  return <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
+                }
+              }
+            </ToggleLikeMutation>
 
-            <Button onClick={() => setLiked(!liked)}>
-              <Icon size='32px' />{likes} likes!
-            </Button>
           </>
       }
 
